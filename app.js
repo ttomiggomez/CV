83fcf24e6b7fbc1fc0111c9494d8e578ddded43d
ttomiggomez <66226013+ttomiggomez@users.noreply.github.com>
2021-12-22 02:00:44 -0300

    Add files via upload
    Se agregan los archivos del CV en formato HTML y CSS.

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+function clicked(){
+
+
+if($('#message').css('display') === 'none'){
+  $('#message').css('display','block');
+} else {
+  $('#message').css('display','none');
+}
+
+
+
+  
+  }
